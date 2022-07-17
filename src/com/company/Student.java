2b76ebf2ba215dd8e2c6319b52ab0f28595cb736
jbiz <pozolotusiko@mail.ru>
2022-07-17 17:52:25 +0300

    java inheritance

---
+++ src/com/company/Student.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Student extends Person {
+    private String major;
+    /**
+    * @param name Имя студента
+    * @param major Специализация студента
+    */
+    public Student(String name, String major) {
+    // передать строку name конструктору суперкласса
+    // в качестве его параметра
+        super(name);
+        this.major = major;
+    }
+    public String getDescription() {
+       return "a student majoring in " + major;
+    }
+}
