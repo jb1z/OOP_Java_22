7520ec01543b55f5a34a998f0f2f8b14c4fcbb5b
jbizLT <pozolotusiko@mail.ru>
2022-07-19 02:05:51 +0300

    considering priority queue

---
+++ src/data_structures/Imp_Priority_Queue.java
@@ -0,0 +1,22 @@
+package data_structures;
+import java.util.PriorityQueue;
+import java.util.Queue;
+
+public class Imp_Priority_Queue {
+    public static void main(String[] args){
+        Queue<Double> priorityQueue = new PriorityQueue<>();
+        priorityQueue.offer(4d);
+        priorityQueue.offer(3d);
+        priorityQueue.offer(2d);
+        priorityQueue.offer(5d);
+        priorityQueue.offer(-1d);
+        priorityQueue.offer(1d);
+        while(!priorityQueue.isEmpty()){
+            System.out.println(priorityQueue.poll());
+        }
+        // the element in priority queue are ordering in ascending order
+
+        //Queue<Double> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        // also we can create a priority queue in reversed order (descending) like this
+    }
+}
