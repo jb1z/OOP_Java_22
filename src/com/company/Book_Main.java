8c4ca3bbe97956cef082c27adc446a157dbed6e8
jbizLT <pozolotusiko1@outlook.com>
2022-07-18 22:25:20 +0300

    enumeration

--- src/com/company/Book_Main.java
+++ src/com/company/Book_Main.java
@@ -1,7 +1,9 @@
 package com.company;
 
 public class Book_Main {
-
+    public enum EnumTest{ // enumeration
+        UP, DOWN, LEFT, RIGHT
+    }
     public static void main(String[] args) {
         Book_Person[] people = new Book_Person[2];
         // заполнить массив people объектами типа Student и Employee
@@ -12,5 +14,7 @@ public class Book_Main {
         for (Book_Person p : people) {
             System.out.println(p.getName() + ',' + p.getDescription());
         }
+        EnumTest enum1 = EnumTest.DOWN;
+        System.out.println(enum1);
     }
 }
