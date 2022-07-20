6faf82a5ef8c7470cfe037647e49703ddec6e111
jbiz <pozolotusiko@mail.ru>
2022-07-20 18:52:28 +0300

    lambdas

---
+++ src/video/Student.java
@@ -0,0 +1,17 @@
+package video;
+
+public class Student {
+    private Specialization specialization;
+
+    Student(Specialization specialization){
+        this.specialization = specialization;
+    }
+
+    public Specialization getSpecialization(){
+        return this.specialization;
+    }
+
+    enum Specialization{
+        ComputerScience, Biology, Astronomy
+    }
+}
